23af19cbb04e5ae4d505b33e7efc897f01040551
edelson amoretti <edelson.amoretti@gmail.com>
2021-04-01 22:48:09 -0500

    primer cambio

---
+++ Scripts/main.js
@@ -0,0 +1,13 @@
+function c (){ 
+    var x;   
+    for(var i = 0; i < 5;i++){
+        //x = i;
+        console.log(i);
+        console.log(x);
+    }
+    console.log(x);
+};
+
+c();
+const x = 5;
+console.log(x);
